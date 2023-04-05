cd422e533dd0c0aeca11cd1d934aba426b81bb77
ayobamiseun <adegokeayobamiseun@gmail.com>
2023-04-05 20:41:16 +0100

    calculate formulae

--- src/components/BMI.js
+++ src/components/BMI.js
@@ -1,28 +1,73 @@
- import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import BodyCalculator from "./BodyCalculator";
 
 export default function BMI() {
-  const [bmiValue, setBmiValue ] = useState(0)
+  const [bmiValue, setBmiValue] = useState(0);
+  const [category, setCategory] = useState("");
+  const [background, setBackground] = useState("")
+
+  useEffect(() => {
+    function bmiCategory() {
+      if (bmiValue >= 1 && bmiValue <= 18.5) {
+        setCategory("Underweight");
+      }
+      if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+        setCategory("Normal weight");
+      }
+      if (bmiValue >= 24.9 && bmiValue <= 29.9) {
+        setCategory("Overweight");
+      }
+      if (bmiValue >= 30) {
+        setCategory("Obese");
+      }
+    }
+    bmiCategory();
+
+    function BmiBackground() {
+      if (bmiValue >= 1 && bmiValue <= 18.5) {
+        setBackground("#FED88B");
+      }
+      if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+        setBackground("#80ff80");
+      }
+      if (bmiValue >= 24.9 && bmiValue <= 29.9) {
+        setBackground("#FF7F50");
+      }
+      if (bmiValue >= 30) {
+        setBackground("#FF5411");
+      }
+    }
+    BmiBackground()
+
+    
+   
+    
+  }, [bmiValue]);
+
+  let bmiStyle = "";
+  if (bmiValue > 0 && category) {
+  
+    bmiStyle = category.split(" ")[0].toLowerCase();
+  }
+  console.log(bmiStyle);
+
   return (
     <>
-      <div className="calculator">
+      <div style={{backgroundColor: background}} className="calculator">
         <h3>Body Mass Index Calculator</h3>
-        <div className="bmi-result-container ">
-        <div className="bmi-result">
-        <div className="bmi-result-number">
-         Body Mass Index number = {bmiValue} 
+        <div className="bmi-result-container">
+          <div className="bmi-result">
+            <div className="bmi-result-number">
+              Body Mass Index number = {bmiValue}
+            </div>
 
+            <div className={`bmi-category ${bmiStyle}`}>
+                 {category}
+            </div>
+          </div>
+          <BodyCalculator setBackground={setBackground} setBmiValue={setBmiValue} />
         </div>
-
-        <div className={`bmi-calculator`}>
-              Under weight
-        </div>
-
-        </div>
-        <BodyCalculator setBmiValue={setBmiValue} /> 
-
-        </div>
-      </div> 
+      </div>
     </>
-  )
+  );
 }
--- src/components/BodyCalculator.jsx
+++ src/components/BodyCalculator.jsx
@@ -1,17 +1,36 @@
 import React, { useState, useEffect } from "react";
 import FormInput from "./FormInput";
 
-export default function BodyCalculator({setBmiValue}) {
+export default function BodyCalculator({ setBmiValue, setBackground }) {
   const [heightUnit, setHeightUnit] = useState("");
   const [weightUnit, setWeightUnit] = useState("");
   const [inchesUnit, setInchesUnit] = useState("");
+  
+  
   const [unit, setUnit] = useState("");
   const [count, setCount] = useState({
     heightCount: "0",
     inchesCount: "0",
     weightCount: "0",
   });
+  function bmiFormular(height, weight) {
+    if (weight > 0 && height > 0) {
+      const heightToMeter = height / 100;
+      const bmi = weight / (heightToMeter * heightToMeter);
+      console.log(bmi);
+      setBmiValue(Math.round(bmi));
+    }
+  }
 
+  function imperialBmi(height, weight, inches) {
+    if (height > 0 && weight > 0 && inches > 0) {
+      // wo since 12 inches make a foot
+      // foot into inches  and then add it to inches value
+      const heightToInches = height * 12 + parseInt(inches);
+      const bmi = 703 * (weight / (heightToInches * heightToInches));
+      setBmiValue(Math.round(bmi));
+    }
+  }
   const { heightCount, inchesCount, weightCount } = count;
 
   useEffect(() => {
@@ -20,11 +39,11 @@ export default function BodyCalculator({setBmiValue}) {
       setHeightUnit("cm");
       setWeightUnit("Kg");
     }
-    bmiFormular(heightCount, weightCount)
-
-  }, [heightCount, weightCount]);
+    bmiFormular(heightCount, weightCount);
+    imperialBmi(heightCount, weightCount, inchesCount);
+  }, [heightCount, weightCount, inchesCount]);
   const onChangeInput = (e) => {
-    const { name, value } = e.target; 
+    const { name, value } = e.target;
     setCount((prevState) => ({
       ...prevState,
       [name]: value,
@@ -33,7 +52,7 @@ export default function BodyCalculator({setBmiValue}) {
 
   const onSelectTag = (e) => {
     setUnit(e.target.value);
-    if (e.target.value === "Metric") { 
+    if (e.target.value === "Metric") {
       setHeightUnit("cm");
       setWeightUnit("Kg");
     } else {
@@ -44,28 +63,20 @@ export default function BodyCalculator({setBmiValue}) {
   };
   function ResetData(e) {
     e.preventDefault();
-    setBmiValue(0)
+    setBmiValue(0);
     setCount({
       heightCount: "0",
-    inchesCount: "0",
-    weightCount: "0",
-    })
+      inchesCount: "0",
+      weightCount: "0",
+    });
     setUnit("Metric");
     setHeightUnit("cm");
     setWeightUnit("Kg");
+    setBackground("")
   }
 
-  function bmiFormular(weight, height) {
-   
-    if(weight > 0 && height > 0)  {
-      const heightToMeter = height / 100
-      const bmi = weight / Math.pow(heightToMeter , 2);
-      console.log(bmi)
-      setBmiValue(Math.round(bmi))
-    }
-    
-  }
-  
+ 
+
   return (
     <>
       <div className="bmi-input">
@@ -94,7 +105,7 @@ export default function BodyCalculator({setBmiValue}) {
           {unit === "Imperial" ? (
             <FormInput
               text="text"
-              name={inchesCount}
+              name="inchesCount"
               title={`Inches (${inchesUnit})`}
               value={inchesCount}
               onChange={onChangeInput}
